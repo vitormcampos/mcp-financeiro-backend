645e833272373f45dcb251390c3a740928958c61
Vitor Campos <vitormcampos99@gmail.com>
2025-12-13 18:58:49 -0300

    refactor: Move projeto angular para repositorio externo

--- WebClient/src/app/models/user-register.ts
+++
@@ -1,5 +0,0 @@
-export class UserRegister {
-  username: string | null | undefined;
-  password: string | null | undefined;
-  confirmPassword: string | null | undefined;
-}
--- WebClient/src/app/models/user-token.ts
+++
@@ -1,5 +0,0 @@
-export class UserToken {
-  token: string | null | undefined;
-}
-
-export const authTokenKey = 'VitorCampos_Financeiro_Token';
