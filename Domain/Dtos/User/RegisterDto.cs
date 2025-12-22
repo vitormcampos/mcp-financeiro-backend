856556850b425ea15544556ab87422fbc0adb0dc
Vitor Campos <vitormcampos99@gmail.com>
2025-12-22 11:08:44 -0300

    refactor(domain): Renomeia e adiciona propriedades para dtos - Renomeia dto para cadastro de conta - Cria dto para login

--- Domain/Dtos/User/RegisterDto.cs
+++ Domain/Dtos/User/RegisterDto.cs
@@ -1,7 +1,8 @@
 namespace Domain.Dtos.User;
 
-public class CreateUser
+public class RegisterDto
 {
     public string Username { get; set; }
     public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
 }
